d70afb6a008fb87dcc88384e8ce8fb9a827f4f4a
lucasmdv <lucasmdv11@gmail.com>
2025-09-08 16:00:28 -0300

    simulacro v0.5

--- simulacro/app.js
+++ simulacro/app.js
@@ -1,6 +1,4 @@
 let url = "http://localhost:3000/extraterrestres"
-let getID = document.getElementById("getId")
-let pGet = document.getElementById("getText")
 let postNombre = document.getElementById("postNombre")
 let postPlaneta = document.getElementById("postPlaneta")
 let postPoder = document.getElementById("postPoder")
@@ -10,22 +8,38 @@ let putNombre = document.getElementById("putNombre")
 let putPlaneta = document.getElementById("putPlaneta")
 let putPoder = document.getElementById("putPoder")
 let pPut = document.getElementById("putText")
-let deleteId = document.getElementById("deleteId")
-let pDelete = document.getElementById("deleteText")
+let lista = document.getElementById("lista-et")
 
 async function getFunction() {
-    let id = getID.value
     try {
-        let response = await fetch(url + "/" + id)
+        let response = await fetch(url)
         let postObtenido = await response.json()
-        pGet.textContent = JSON.stringify(postObtenido,null,2)
         console.log(postObtenido)
+        return postObtenido
     } catch (error) {
         pGet.textContent = "Error: " + error
+        return []
     }
 }
 
-document.getElementById("getBtn").addEventListener("click",getFunction)
+async function agregarALista() {
+    let aliens = await getFunction();
+    lista.innerHTML = "";
+    aliens.forEach(element => {
+        const li = document.createElement('li');
+        li.textContent = `ID: ${element.id} -- Nombre: ${element.nombre} -- Planeta: ${element.planeta} -- Nivel de poder: ${element.nivelPoder} `;
+
+        const btnEliminar = document.createElement("button");
+        btnEliminar.textContent = "Eliminar";
+        btnEliminar.addEventListener("click",async () => {
+            await deleteFunction(element.id)
+            agregarALista()
+        })
+
+        li.appendChild(btnEliminar);
+        lista.appendChild(li);
+    });
+}
 
 async function postFunction() {
     try {
@@ -43,6 +57,8 @@ async function postFunction() {
         let data = await response.json()
         pPost.textContent = "Post creado: "+JSON.stringify(data,null,2)
         console.log(data)
+        agregarALista()
+        document.getElementById("postForm").reset()
     } catch (error) {
         pPost.textContent = "Error: " + error
     }
@@ -65,18 +81,33 @@ async function putFunction() {
         if (!response.ok) {
             throw new Error("en el servidor: " + response.status);
         }
-        let data =await response.json()
+        let data = await response.json()
         pPut.textContent = "post elegido ha sido reemplazado por los siguientes datos: "+JSON.stringify(data,null,2)
         console.log(data)
+        agregarALista()
+        document.getElementById("putForm").reset()
     } catch (error) {
         pPut.textContent = error
     }
 }
 
-async function deleteFunction() {
-
-    let response = fetch(url + "/" + deleteId.value, {
-        method: "DELETE"
-    })
-    pDelete.textContent = response.ok ? "Post: " + deleteId.value + " eliminado." : "Error al eliminar el post"
+async function deleteFunction(id) {
+    try {
+        let response = await fetch(url + "/" + id, {
+            method: "DELETE"
+        });
+        if (response.ok) {
+            pDelete.textContent = "Post: " + id + " eliminado.";
+        } else {
+            pDelete.textContent = "Error al eliminar el post";
+        }
+    } catch (error) {
+        pDelete.textContent = "Error: " + error;
+    }
 }
+
+agregarALista()
+
+
+document.getElementById("postBtn").addEventListener("click",postFunction)
+document.getElementById("putBtn").addEventListener("click",putFunction)
