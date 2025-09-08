ebc37595637251292d1fcb6f203e47e6cc3e9e0f
lucasmdv <lucasmdv11@gmail.com>
2025-09-08 10:55:41 -0300

    simulacro

---
+++ simulacro/app.js
@@ -0,0 +1,80 @@
+let url = "http://localhost:3000/extraterrestres"
+let getID = document.getElementById("getId")
+let pGet = document.getElementById("getText")
+let postNombre = document.getElementById("postNombre")
+let postPlaneta = document.getElementById("postPlaneta")
+let postPoder = document.getElementById("postPoder")
+let pPost = document.getElementById("postText")
+let putId = document.getElementById("putId")
+let putNombre = document.getElementById("putNombre")
+let putPlaneta = document.getElementById("putPlaneta")
+let putPoder = document.getElementById("putPoder")
+let pPut = document.getElementById("putText")
+let deleteId = document.getElementById("deleteId")
+let pDelete = document.getElementById("deleteText")
+
+async function getFunction() {
+    let id = getID.value
+    try {
+        let response = await fetch(url + "/" + id)
+        let postObtenido = await response.json()
+        pGet.textContent = JSON.stringify(postObtenido,null,2)
+        console.log(postObtenido)
+    } catch (error) {
+        pGet.textContent = "Error: " + error
+    }
+}
+
+document.getElementById("getBtn").addEventListener("click",getFunction)
+
+async function postFunction() {
+    try {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "aplication/json"
+            },
+            body: JSON.stringify({
+                nombre: postNombre.value,
+                planeta: postPlaneta.value,
+                nivelPoder: Number(postPoder.value)
+            })
+        })
+        let data = await response.json()
+        pPost.textContent = "Post creado: "+JSON.stringify(data,null,2)
+        console.log(data)
+    } catch (error) {
+        pPost.textContent = "Error: " + error
+    }
+}
+
+async function putFunction() {
+    try {
+        let response = await fetch(url + "/" + putId.value, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "aplication/json"
+            },
+            body: JSON.stringify({
+                id: putId.value,
+                nombre: putNombre.value,
+                planeta: putPlaneta.value,
+                nivelPoder: Number(putPoder.value)
+            })
+        })
+        let data =response.json()
+        pPost.textContent = "post elegido ha sido reemplazado por los siguientes datos: "+JSON.stringify(data,null,2)
+        console.log(data)
+    } catch (error) {
+        pPost.textContent = "Error: " + error
+    }
+}
+
+async function deleteFunction() {
+
+    let response = fetch(url + "/" + deleteId.value, {
+        method: "DELETE"
+    })
+
+    pDelete = response.ok ? "Post: " + deleteId.value + " eliminado." : "Error al eliminar el post"
+}
